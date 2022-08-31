1489075adb07b60b5aa39ec62f46e4b3a7ab3848
j4m3s 4l4r1c <jamesalaric@gmail.com>
2022-08-31 14:43:34 +0100

    login update
    first commit of responsive modification

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -7,38 +7,40 @@ import styles from'./Login.module.scss';
 import Input from '@components/base/Input';
 
 const Login = () =>(
-    <div className={styles.container}>
-        {/* image side */}
-        <div className={styles.imageSide}> 
-            <div className={styles.overlayImg}>
-                <img src={img1} alt="log in"/>
+    <div>
+        <div className={styles.logo}>Logo</div>
+        <div className={styles.container}>
+            {/* image side */}
+            <div className={styles.imageSide}> 
+                <div className={styles.overlayImg}>
+                    <img src={img1} alt="log in"/>
+                </div>
+            </div>
+            {/* Form side */}
+            <div className={styles.loginSide}>
+                <h3> Login as a Admin User</h3>
+                <Form>
+                    <Form.Item> <Input placeholder="johndoe@xyz.com" suffix={<AiOutlineUser/>}/></Form.Item>
+                    <Form.Item> <Input.Password placeholder="xxxxxxxxx"  required/> </Form.Item>
+                    <Form.Item>
+                        <Button className={styles.button} type='primary'  htmlType="submit">
+                        LOGIN
+                        </Button>
+                    </Form.Item>
+                    <div className={styles.pwd}><Button style={{color:'gray'}} type="link" htmlType="button">
+                        Forget your password?
+                    </Button></div>
+                    <div className={styles.help}><Button style={{color:'#a457c5'}} type="link" htmlType="button">
+                        <b>Get help signed in.</b>
+                    </Button>
+                    </div>
+                    <div className={styles.terms}><Button style={{color:'gray'}} type="link" htmlType="button">
+                        Terms of use. Privacy policy
+                    </Button></div>
+                </Form>
             </div>
         </div>
-        {/* Form side */}
-        <div className={styles.loginSide}>
-            <h3> Login as a Admin User</h3>
-            <Form>
-                <Form.Item> <Input placeholder="johndoe@xyz.com" suffix={<AiOutlineUser/>}/></Form.Item>
-                <Form.Item> <Input.Password placeholder="xxxxxxxxx"  required/> </Form.Item>
-                <Form.Item>
-                    <Button className={styles.button} type='primary'  htmlType="submit">
-                    LOGIN
-                    </Button>
-                </Form.Item>
-                <div className={styles.pwd}><Button style={{color:'gray'}} type="link" htmlType="button">
-                    Forget your password?
-                </Button></div>
-                <div className={styles.help}><Button style={{color:'#a457c5'}} type="link" htmlType="button">
-                    <b>Get help signed in.</b>
-                </Button>
-                </div>
-                <div className={styles.terms}><Button style={{color:'gray'}} type="link" htmlType="button">
-                    Terms of use. Privacy policy
-                </Button></div>
-            </Form>
-        </div>
     </div>
-
 );
 
 export default Login;
